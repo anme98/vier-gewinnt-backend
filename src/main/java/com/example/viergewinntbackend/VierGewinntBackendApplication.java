dcdcec359e0540342349385f6b5f4340c97f5407
Anel <44476653+anme98@users.noreply.github.com>
2022-05-10 09:51:41 +0200

    git init

---
+++ src/main/java/com/example/viergewinntbackend/VierGewinntBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.viergewinntbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VierGewinntBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VierGewinntBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/viergewinntbackend/VierGewinntBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viergewinntbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VierGewinntBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
